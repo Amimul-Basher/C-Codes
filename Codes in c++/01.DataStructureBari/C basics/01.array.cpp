7a6b27953a0bf3de660b309a2236f534bae0426f
Amimul Basher <amimul.basher@bjitgroup.com>
2023-05-19 19:29:27 +0600

    Structure and Array passing examples added

--- Codes in c++/01.DataStructureBari/C basics/01.array.cpp
+++ Codes in c++/01.DataStructureBari/C basics/01.array.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[10] = {2};
-    for(int i = 0 ;  i < sizeof(arr)/sizeof(int); i++){
-        printf("%d\n", arr[i]);
+    int arr[10] = {1,2,3,4,5,6,7,8,9,0};
+    cout << "Size of array: "<< sizeof(arr)/sizeof(int)<< endl;
+    for(int x: arr){
+        cout << x << " ";
     }
-
-    //for each loop in c++
-    cout << "Printing array with for each loop\n";
-    for(int x:arr){
-        cout << x << endl;
+    cout << "\nEnter the size of array you want: ";
+    int n;
+    cin >>n;
+    int A[n]= {1,3,4}; //it was not possible to declare array with dynamic values in the older version of c++ compiler
+    for(int x: A){
+        cout << x << " ";
     }
-    
     return 0;
 }
