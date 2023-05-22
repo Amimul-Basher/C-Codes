9678f16fa8fc4e86f6b0a73a539b682517558895
Amimul Basher <amimul.basher@bjitgroup.com>
2023-05-22 14:53:00 +0600

    Git Basics tutorial practice examples is completed

--- Codes in c++/01.DataStructureBari/C basics/22.StructureToClass.cpp
+++ Codes in c++/01.DataStructureBari/C basics/22.StructureToClass.cpp
@@ -1,30 +1,45 @@
 #include<iostream>
 using namespace std;
 
-
 class Rectangle{
     private:
     int length;
     int width;
 
     public:
+    //constructor declaration
     Rectangle(int m, int n){
         this->length = m;
         this->width = n;
     }
-    void changeLength(int length){
+    void setLength(int length){
         this->length = length;
     }
+    void setWidth(int width);
 
     void showArea(){
         cout<< "The area of the Rectangle is "<< this->length* this->width << endl;
     }
+
+    ~Rectangle();
+    
 };
 
+//Using Scope resolution (::) we can declare function out of its Class scope 
+void Rectangle::setWidth(int width){
+        this->width = width;
+}
+Rectangle::~Rectangle(){
+    cout << "Destructor" << endl;
+};
+
+
 int main(){
+    //Class declaration and initialization
     Rectangle r(10, 5);
     r.showArea();
-    r.changeLength(15);
+    r.setLength(15);
+    r.setWidth(10);
     r.showArea();
     return 0;
 }
