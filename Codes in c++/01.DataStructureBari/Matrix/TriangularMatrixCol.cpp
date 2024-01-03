dd33e7f804aeff7dc867a1278803765a39b04062
Md Amimul Basher <mdbasher.web@gmail.com>
2024-01-02 23:19:10 -0500

    Sparse Matrix addition subtraction and representation code added

--- Codes in c++/01.DataStructureBari/Matrix/TriangularMatrixCol.cpp
+++ Codes in c++/01.DataStructureBari/Matrix/TriangularMatrixCol.cpp
@@ -23,21 +23,25 @@ class TriangularMatrix{
 };
 
 void TriangularMatrix::set(int i, int j, int value){
-    int x = (n*(n+1))/2;
-    int y = n-j;
-    cout << (x-((y*(y+1)/2)))+(i-j) << endl;
+    // int x = (n*(n+1))/2;
+    // int y = n-j;
+    // cout << (x-((y*(y+1)/2)))+(i-j) << endl;
     if(j <= i) 
-        A[(x-((y*(y+1)/2)))+(i-j)] = value;
+        // A[(x-((y*(y+1)/2)))+(i-j)] = value;
+        A[((n*j-(j*(j-1)/2))+(i-j))] = value;
 }
 
 void TriangularMatrix::display(){
 
-    int x = (n*(n+1))/2;
-    int y = 0;
+    // int x = (n*(n+1))/2;
+    // int y = 0;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            y = n-j;
-            if(j <= i) cout << A[(x-((y*(y+1)/2)))+(i-j)] << " " ;
+            // y = n-j;
+            if(j <= i) {
+                // cout << A[(x-((y*(y+1)/2)))+(i-j)] << " " ;
+                cout << A[((n*j-(j*(j-1)/2))+(i-j))] << " ";
+            }
             else cout << 0 << " ";
         }
         cout << endl;
@@ -51,12 +55,12 @@ void TriangularMatrix::display(){
 int main(){
 
     TriangularMatrix t(5);
-    t.set(0, 0, 4);
-    t.set(1, 1, 4);
+    t.set(0, 0, 3);
+    t.set(1, 1, 8);
     t.set(2, 0, 4);
-    t.set(2, 1, 4);
+    t.set(2, 1, 2);
     t.set(2, 2, 4);
-    t.set(3, 3, 4);
+    t.set(3, 3, 1);
     
     t.display();
 
